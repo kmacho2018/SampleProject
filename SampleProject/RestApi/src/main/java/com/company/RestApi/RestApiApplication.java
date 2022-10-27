90198002d3c22275422a8a180ff40608af0123c7
CODETEL\Juan Camacho <juan_camacho@claro.com.do>
2022-10-27 19:15:27 -0400

    agregando apellido

--- SampleProject/RestApi/src/main/java/com/company/RestApi/RestApiApplication.java
+++ SampleProject/RestApi/src/main/java/com/company/RestApi/RestApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /***
- * @author  Alexandra Cruz
+ * @author  Alexandra Cruz Javier
  */
 @SpringBootApplication
 public class RestApiApplication {
